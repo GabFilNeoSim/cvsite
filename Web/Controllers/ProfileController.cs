98bff23572ccbb0a76feb95bdb05e2abf4d13df5
Filip Siri <filipsiri03@hotmail.com>
2024-12-19 10:57:00 +0100

    Progress on  stuff

---
+++ Web/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers;
+
+public class ProfileController : Controller
+{
+    private readonly ILogger<ProfileController> _logger;
+    private readonly AppDbContext _context;
+
+    public ProfileController(ILogger<ProfileController> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
