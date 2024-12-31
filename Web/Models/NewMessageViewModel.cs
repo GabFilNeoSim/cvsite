72fab6332e68ba31dfcbeffa544d460d1ce4b67a
N30A <95051018+N30A@users.noreply.github.com>
2024-12-31 10:03:47 +0100

    Started message functionality

---
+++ Web/Models/NewMessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace Web.Models;
+
+public class NewMessageViewModel
+{
+    public required string Text { get; set; }
+    public string? AnonymousName { get; set; }
+    public required User Receiver {  get; set; }
+}
