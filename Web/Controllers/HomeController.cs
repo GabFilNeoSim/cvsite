b29a34d1af20501b0e0ccc6c6a5b459a28ea637d
Simon Persson <simon@karlsimon.se>
2025-01-01 15:20:31 +0100

    Removed old logic

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -13,12 +13,6 @@ public class HomeController : BaseController
 
     public IActionResult Index() => View();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });    
-    }
-
     [HttpGet("home/users")]
     public IActionResult SearchUsers([FromQuery] string query)
     {
