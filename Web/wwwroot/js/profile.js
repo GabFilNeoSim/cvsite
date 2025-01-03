072c8ddf06519ae9c5bc47bb8b71b41d146bc8bd
Filip Siri <filipsiri03@hotmail.com>
2025-01-03 15:09:21 +0100

    start on anonymous msg

--- Web/wwwroot/js/profile.js
+++ Web/wwwroot/js/profile.js
@@ -4,8 +4,14 @@ const input = document.getElementById("profile-change-avatar-input");
 
 avatar.addEventListener("click", () => input.click());
 
-input.addEventListener("change", () => {
-    if (input.files.length > 0) {
-        form.submit();
-    }
+console.log("Hello world");
+
+
+$("#profile-anonymous-message-btn").on("click", function () {
+    $("#message-container").show();
+});
+
+
+$("#message-modal-close").on("click", function () {
+    $("#message-container").hide();
 });
