68022857b427e088941e58f1557a9f43169d4587
Filip Siri <filipsiri03@hotmail.com>
2025-01-06 11:09:25 +0100

    uncomment

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -5,28 +5,28 @@
     }
 }
 
-//let currentProfileIndex = 1;
-//let profileMoveLength = 210;
-//const profilecards = $(".card");
+let currentProfileIndex = 1;
+let profileMoveLength = 210;
+const profilecards = $(".card");
 
-//$("#left").on('click', function (event) {
-//    moveProfileCarousel(-1);
-//});
-//$("#right").on('click', function (event) {
-//    moveProfileCarousel(1);
-//});
+$("#left").on('click', function (event) {
+    moveProfileCarousel(-1);
+});
+$("#right").on('click', function (event) {
+    moveProfileCarousel(1);
+});
 
-//function moveProfileCarousel(direction) {
-//    currentProfileIndex = (currentProfileIndex + direction) % profilecards.length;
-//    updateProfileCardStyling();
-//}
+function moveProfileCarousel(direction) {
+    currentProfileIndex = (currentProfileIndex + direction) % profilecards.length;
+    updateProfileCardStyling();
+}
 
-//function updateProfileCardStyling() {
-//    profilecards.each(function (index, item) {
-//        const relativeIndex = Math.abs((item.id - currentProfileIndex) % profilecards.length);
-//        item.style.transform = `translateX(${relativeIndex * profileMoveLength}px)`;
-//    })
-//}
+function updateProfileCardStyling() {
+    profilecards.each(function (index, item) {
+        const relativeIndex = Math.abs((item.id - currentProfileIndex) % profilecards.length);
+        item.style.transform = `translateX(${relativeIndex * profileMoveLength}px)`;
+    })
+}
 
 window.onload = scrollToBottom;
 window.onload = updateProfileCardStyling;
